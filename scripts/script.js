d2093e2dc56dd605b40d008ca67bc3cc340f1c53
ReemShkeep <shkeepreem@gmail.com>
2023-01-31 20:29:46 +0200

    again

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,3 @@
-//TODO:
-/*
-    -Create events to detect keyboard hits
-        and move the paddle accordingly.
-    - Class Sprite
-    - Class Paddle
-    - Create objects
-    - final objective move the paddle
-
-*/
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 1024;
@@ -146,38 +135,3 @@ function draw() {
 }
 
 draw();
-///////////////////////////////////////////////////
-
-const sprite = new Sprite({
-  height: 30,
-  width: 200,
-  position: {
-    x: 50,
-    y: canvas.height - 60,
-  },
-  velocity: {
-    x: 0,
-    y: 0,
-  },
-});
-
-
-
-function animate() {
-  window.requestAnimationFrame(animate);
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-  sprite.velocity.x = 5;
-  sprite.update();
-}
-
-animate();
-
-/*
-update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  }
-  */
